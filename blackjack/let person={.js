dc65074bbceae09803cc959cd296ab11c9c3f242
oktaytezcan <oktaytezcan9197@gmail.com>
2022-12-08 16:49:17 +0300

    Blackjack Game

---
+++ blackjack/let person={.js
@@ -0,0 +1,13 @@
+let person={
+    name:"oktay",
+    age:25,
+    country:"turkey"
+}
+
+function logData(){
+
+person.name+"is " +person.age +"years old and lives in" +person.country
+
+
+}
+logData()
